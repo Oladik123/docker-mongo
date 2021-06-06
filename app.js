e7bb2a6d51612d79c7c9bb4939d35c106aa613f8
VitchenkoVladislav <v.vitchenko@softaria.com>
2021-06-07 03:59:26 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://user:password@mongo:27017/";
+const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+
+mongoClient.connect((err, client) => {
+
+    const db = client.db("usersdb");
+    const collection = db.collection("users");
+    let user = {name: "Tom", age: 23};
+    collection.insertOne(user, (err, result) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log(result.ops);
+        client.close();
+    });
+});
